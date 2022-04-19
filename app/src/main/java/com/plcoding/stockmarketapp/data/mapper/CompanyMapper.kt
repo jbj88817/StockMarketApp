4802d19915545f5e65b8a90f720b63dc50dec294
Bojie Jiang <jiangbojie@gmail.com>
2022-04-18 22:02:28 -0700

    cp2

--- app/src/main/java/com/plcoding/stockmarketapp/data/mapper/CompanyMapper.kt
+++ app/src/main/java/com/plcoding/stockmarketapp/data/mapper/CompanyMapper.kt
@@ -3,7 +3,7 @@ package com.plcoding.stockmarketapp.data.mapper
 import com.plcoding.stockmarketapp.data.local.CompanyListingEntity
 import com.plcoding.stockmarketapp.domain.model.CompanyListing
 
-fun CompanyListing.toCompanyListing(): CompanyListing {
+fun CompanyListingEntity.toCompanyListing(): CompanyListing {
     return CompanyListing(
         name = name,
         symbol = symbol,
