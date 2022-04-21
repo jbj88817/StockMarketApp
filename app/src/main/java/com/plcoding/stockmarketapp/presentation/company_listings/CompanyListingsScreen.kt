1e04b5791ce0fadcaf7722d6ad2928c45dc8fb91
Bojie Jiang <jiangbojie@gmail.com>
2022-04-20 22:52:27 -0700

    CompanyInfoScreen

--- app/src/main/java/com/plcoding/stockmarketapp/presentation/company_listings/CompanyListingsScreen.kt
+++ app/src/main/java/com/plcoding/stockmarketapp/presentation/company_listings/CompanyListingsScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
+import com.plcoding.stockmarketapp.presentation.destinations.CompanyInfoScreenDestination
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
@@ -52,7 +53,11 @@ fun CompanyListingsScreen(
                     CompanyItem(company = company,
                         Modifier
                             .fillMaxWidth()
-                            .clickable { }
+                            .clickable {
+                                navigator.navigate(
+                                    CompanyInfoScreenDestination(company.symbol)
+                                )
+                            }
                             .padding(16.dp)
                     )
                     if (idx < state.companies.size) {
